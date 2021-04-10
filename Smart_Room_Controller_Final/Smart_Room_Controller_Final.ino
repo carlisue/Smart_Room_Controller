52095a5230a4e801fb64098b9b4fd662e5893f78
Carli Stringfellow <carlisue1@gmail.com>
2021-04-10 13:28:30 -0600

    Default mode fully works, but relax mode is only getting called once for a short second

--- Smart_Room_Controller_Final/Smart_Room_Controller_Final.ino
+++ Smart_Room_Controller_Final/Smart_Room_Controller_Final.ino
@@ -1,29 +1,21 @@
 /*
  * Project:     Smart_Room_Controller: Cohort 4 Midterm
- * Description: Controls two modes. Default mode (working) and Break mode
+ * Description: Controls two modes: Default mode (working) and Break mode
  *              to encourge a worker to get up from desk and move their body.
  * Author:      Carli Stringfellow
  * Date:        04-09-2021
  */
  
 #include <OneButton.h>
-#include <Adafruit_NeoPixel.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <Encoder.h>
 #include <mac.h>
 #include <wemo.h>
 #include <hue.h>
-#include <colors.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_BME280.h>
-
-//pixeldeclarations
-const int PIXELPIN = 20;
-const int PIXELCOUNT = 12;
-int p;
-Adafruit_NeoPixel pixel(PIXELCOUNT, PIXELPIN, NEO_GRB + NEO_KHZ800);
 
 int currentTime;
 int lastSecond;
@@ -79,27 +71,22 @@ bool manualFan;
  const int HUEMIN = 0;
  const int HUEMAX = 12;
  int HueBri;
- int b;
+ int b = 0;
  int HueWork[] = {HueBlue, HueGreen, HueBlue, HueIndigo, HueGreen};
  int HueRelax[] = {HueRed, HueYellow, HueOrange, HueRed, HueYellow};
 
  // Timer Declarations
  unsigned long startTimer;
- bool timerON;
- const int sittingDistance = 30;
- const int sittingTime = 5000;
+ bool timerON = true;
+ const int sittingDistance = 50;
+ const int sittingTime = 10000;
  
 void setup() {
-  pixel.begin();
-  pixel.show();
-  pixel.setBrightness(73);
-  
   Serial.begin(9600); //Turns on serial monitor
   bme.begin(0x76); // Turns on bme sensor
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-  display.setRotation(rot); // Sets rotation to
-  rot = 0;
+  display.setRotation(rot); // Sets rotation to rot = 0
   lastDistance = 0; 
 
   
@@ -155,21 +142,26 @@ void loop() {
   yellowButton.tick(); // checks fan buttonstate
  
   encoderButton.tick(); // encoder controls brightness and toggles hues on/off 
-  
-//  breakMode();
-  defaultMode(); // calls default mode which sets hues to HueWork and toggles default functions
+//    if(!encoderButtonState) {
+//    setHue(b, false, HueWork[b-1], 0, 0); 
+//    }
+//    
+      defaultMode(); // calls work mode 
+//    breakMode();
 
-      if(timerON) {
+    if(timerON) {
     if((millis() - startTimer) > sittingTime) {
       if(inches <= sittingDistance) {
         breakMode();
-        timerON = false;
+        Serial.printf("timer: \n", timerON);
       }
-    }
+      timerON = false;
+    } 
+   
   }
 
 }
-void orangeClick() { // Toggles teapot
+void orangeClick() {
   orangeButtonState = !orangeButtonState;
     if(orangeButtonState) {
       switchON(TEAPOT);
@@ -180,7 +172,7 @@ void orangeClick() { // Toggles teapot
 //  Serial.printf("orangeButtonState = %i", orangeButtonState);
 }
 
-void yellowClick() { // Toggles fan
+void yellowClick() { // toggles fan
   yellowButtonState = !yellowButtonState;
   manualFan = true;
        if(yellowButtonState) {
@@ -195,61 +187,9 @@ void yellowClick() { // Toggles fan
 void encoderClick() { // turns hues off
   encoderButtonState = !encoderButtonState;
   Serial.printf("encoderButtonState = %i\n", encoderButtonState);
+
 }
 
-void breakMode() { // automatic mode triggered by ultrasonic wave in inches
-  display.clearDisplay();
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw WHITE text
-  display.setCursor(0,0);             // Start at top-left corner
-
-  display.printf("\n");
-  display.printf("Time to Relax\n");
-  display.printf("\n");
-  display.printf("Take a Break\n");
-  display.printf("\n");
-  display.printf("STRETCH YOUR BODY\n");
-  display.printf("\n");
-  display.display();
-
-  setHue(b, true, HueRelax[b-1], 255, 255);
-  
-  pixel.clear();
-  for(p=0; p<13; p++) {
-    pixel.setPixelColor(p, orange);
-    delay(500);
-    pixel.show();
-  }
-
- }
-
-void resetToDefault() { // Uses longpress on the encoder to reset to default mode
-  defaultMode();
-  manualFan = false;
-  timerON = true;
-  firstRelax = true;
-  startTimer = millis();
-}
-void ultraSonic() {
-  //Sends ultrasonic pulse
-  digitalWrite(trigPin, LOW); // Clears the trigPin condition
-  delayMicroseconds(2);
-  
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculates the distance
-  distance = duration * 0.34 / 2; // time passed * speed of sound divided by 2 (go and back)
-  dist_cm = distance / 10;
-  inches = dist_cm * 0.3937;
-//  Serial.printf("Distance:\n\n%i mm\n%i cm\n%0.02f inches\n\n", distance, dist_cm, inches);
-  
-    lastDistance = distance;
-}
 void defaultMode() { //this is work mode as the most time is spent here
   display.clearDisplay();
   display.setTextSize(1);             // Normal 1:1 pixel scale
@@ -263,8 +203,7 @@ void defaultMode() { //this is work mode as the most time is spent here
   humidRH = bme.readHumidity(); // displays humidity
   display.printf("Humidity is %0.1f\n" ,humidRH);
 //  Serial.printf("Humidity is %0.1f\n" ,humidRH);
-//  Serial.printf("Distance:\n%0.1f inches\n", inches);
-  display.printf("Distance:\n%0.1f inches\n", inches);
+
   display.printf("\n");
   display.printf("\n");
 
@@ -294,10 +233,10 @@ void defaultMode() { //this is work mode as the most time is spent here
   currentTime = millis();
     if((currentTime-lastSecond) > 1000) {
       for(b=1; b<=5; b++) {
-        if(encoderButtonState == true) { //Starts the default mode with green on and hue lights
+        if(encoderButtonState == true) {
           digitalWrite(REDLED,LOW);
           digitalWrite(GREENLED,HIGH);
-          setHue(b, true, HueWork[b-1], HueBri*20, 255);
+          setHue(b, true, HueWork[b-1], HueBri*20, 190);
           }
         else {
           digitalWrite(REDLED,HIGH);
@@ -307,13 +246,78 @@ void defaultMode() { //this is work mode as the most time is spent here
       }
         lastSecond = millis();
     }
-       
+          
 //    if(timerON) {
 //    if((millis() - startTimer) > sittingTime) {
 //      if(inches <= sittingDistance) {
 //        breakMode();
 //        timerON = false;
+//        Serial.printf("timer: \n", timerON);
 //      }
 //    }
 //  }
+}
+
+
+void breakMode() { // automatic mode triggered by ultrasonic wave in inches
+  display.clearDisplay();
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw WHITE text
+  display.setCursor(0,0);             // Start at top-left corner
+  
+  display.printf("\n");
+  display.printf("Time to Relax\n");
+  display.printf("\n");
+  display.printf("Take a Break\n");
+  display.printf("\n");
+  display.printf("STRETCH YOUR BODY\n");
+  display.printf("\n");
+  display.display();
+
+  for(b=1; b<=5; b++) {
+    setHue(b, true, HueRelax[b-1], 255, 255);
+  }
+
+//  if(firstRelax) { // Flashes hue lights for first relax
+//   for(int i = 0; i >= 3; i++) {
+//    setHue(b, false, HueRelax[b-1], 0, 0);
+//    delay(1000);
+//    setHue(b, true, HueRelax[b-1], 255, 255);
+//    delay(1000);
+//    setHue(b, false, HueRelax[b-1], 0, 0);
+//    delay(1000);
+//    setHue(b, true, HueRelax[b-1], 195, 255);
+//      firstRelax = false;
+//   }
+//  }
+ }
+
+void resetToDefault() { // Uses longpress on the encoder to reset to default mode
+  defaultMode();
+  manualFan = false;
+  timerON = true;
+  firstRelax = true;
+  startTimer = millis();
+}
+void ultraSonic() {
+  //Sends ultrasonic pulse
+  digitalWrite(trigPin, LOW); // Clears the trigPin condition
+  delayMicroseconds(2);
+  
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculates the distance
+  distance = duration * 0.34 / 2; // time passed * speed of sound divided by 2 (go and back)
+  dist_cm = distance / 10;
+  inches = dist_cm * 0.3937;
+  
+  // Displays the distance on the Serial Monitor
+ 
+//    Serial.printf("Distance:\n\n%i mm\n%i cm\n%0.02f inches\n\n", distance, dist_cm, inches);
+    lastDistance = distance;
 }
